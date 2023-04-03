8a645663119136445d91b39728b413f77904a457
Eutarides <javifarmacastillo@gmail.com>
2023-04-03 13:52:41 +0200

    modal JS complete and CSS completely done

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,32 @@
 export default (() => {
     const botones = document.querySelectorAll('.modal-button');
-    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
 
     botones.forEach((boton)=>{
         boton.addEventListener('click', ()=> {
-            modal.classList.toggle('active');
+            overlay.classList.toggle('active');
         })
 
     });
-  
+
+    const cross = document.querySelector('.checkout-cross');
+
+    cross.addEventListener('click', ()=> {
+        overlay.classList.toggle('active');
+    });
+
+    let modal = document.querySelector('.modal-column');
+    let final = document.querySelector('.modal-item-button');
+
+    final.addEventListener('click', ()=> {
+        modal.classList.toggle('active');
+    });
+
+    let volver = document.querySelector('.goback-button');
+
+    volver.addEventListener('click' , ()=> {
+        modal.classList.toggle('active');
+    });
+
+
 })();
